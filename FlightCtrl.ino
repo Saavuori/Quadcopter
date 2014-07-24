046630ee95a5091ed0855d6fb83d9f71f36af550
SakkeS <sampsa.saavuori@gmail.com>
2014-07-24 13:33:21 +0300

    Update FlightCtrl.ino

--- FlightCtrl.ino
+++ FlightCtrl.ino
@@ -1,21 +1,22 @@
 void FlightControl(){
-
-  int PIDroll_val  = (int)PIDroll.Compute((float)setRoll-gy_aver);
-  int PIDpitch_val = (int)PIDpitch.Compute((float)setPitch-gx_aver);
-  int PIDyaw_val   = (int)PIDyaw.Compute((float)setYaw-gz_aver);
-
-  m1_val=throttle+PIDroll_val+PIDpitch_val+PIDyaw_val;
-  m2_val=throttle-PIDroll_val+PIDpitch_val-PIDyaw_val;
-  m3_val=throttle+PIDroll_val-PIDpitch_val-PIDyaw_val;
-  m4_val=throttle-PIDroll_val-PIDpitch_val+PIDyaw_val;
-
-  analogWrite(MOTOR1,m1_val);
-  analogWrite(MOTOR2,m2_val);
-  analogWrite(MOTOR3,m3_val);
-  analogWrite(MOTOR4,m4_val);
   
-  
-
+  /*
+      if((millis()-time) > ALARM_LOW && !alarm )
+      {
+          setPitch=0;
+          setRoll=0;
+          alarm = true;
+          Serial1.println("#AlarmLow;");
+      }  
+      if((millis()-time)>ALARM_HIGH && millis()-timer>1000)
+      {  
+          timer = millis();   
+          throttle = throttle>MOTOR_ZERO_LEVEL?throttle*DESCEND_RATE:MOTOR_ZERO_LEVEL;   //todo: if baro decend
+          Serial1.println("#Alarmhigh;");
+      }
+  */
+  motorWrite(m1_val,m2_val,m3_val,m4_val);
+    
 }
 
 
