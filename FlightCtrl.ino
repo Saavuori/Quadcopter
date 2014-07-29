397d19d746bad565859257ef65b2dff02d46f3f5
SakkeS <sampsa.saavuori@gmail.com>
2014-07-29 14:07:35 +0300

    Update FlightCtrl.ino

--- FlightCtrl.ino
+++ FlightCtrl.ino
@@ -1,7 +1,7 @@
 void FlightControl(){
   
   
-      if((millis()-time) > ALARM_LOW && !alarm )
+      if((millis()-time) > ALARM_LOW && !alarm )      // LOW ALARM
       {
           setPitch=0;
           setRoll=0;
@@ -9,13 +9,32 @@ void FlightControl(){
           alarm = true;
           Serial1.println("#AlarmLow;");
       }  
-      if((millis()-time)>ALARM_HIGH && millis()-timer>1000)
+      if((millis()-time)>ALARM_HIGH && millis()-timer>1000) // HIGH ALARM
       {  
           LED_ON(LED_R);
           timer = millis();   
           throttle = throttle>MOTOR_ZERO_LEVEL?throttle*DESCEND_RATE:MOTOR_ZERO_LEVEL;
           Serial1.println("#Alarmhigh;");
       }
+      
+      
+      if( abs(Pitch - setPitch) < 10 || abs(Yaw - setYaw) < 10 )
+      {
+          angleTime = millis();
+      }
+          
+      if( millis() - angleTime > 2000)
+      {
+        Serial1.println("Angle and setpoint time alarm!");
+        run = false;
+      }
+          
+          
+          
+  
+      
+      
+      
   
   motorWrite(m1_val,m2_val,m3_val,m4_val);
     
